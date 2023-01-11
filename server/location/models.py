29428861148151ebceb21028400b96cb2ea4495a
Moshe Haiem <mhaiem9@gmail.com>
2023-01-10 18:56:53 -0800

    finish up setting boiler plate server

--- server/location/models.py
+++ server/location/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    address = models.CharField(max_length=120)
+    city = models.CharField(max_length=120)
+    state = models.CharField(max_length=120)
+    zip = models.CharField(max_length=120)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    athletes_present = models.IntegerField()
+    athletes_needed = models.IntegerField()
+    start_time = models.DateTimeField(blank=False, null=False)
+    end_time = models.DateTimeField(blank=False, null=False)
+    message = models.TextField()
--- server/location/views.py
+++ server/location/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import LocationSerializer
+from .models import Location
 
-# Create your views here.
+
+class LocationView(viewsets.ModelViewSet):
+    serializer_class = LocationSerializer
+    queryset = Location.objects.all()
