81b1f6bf388d4ce38381b033fabd3f2372585ad9
Moshe Haiem <mhaiem9@gmail.com>
2023-01-11 18:46:15 -0800

    setting up map

---
+++ web/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import MainPage from './views/Main/MainPage';
+
+const App = (): JSX.Element => {
+  return (
+    <MainPage />
+  );
+}
+
+export default App;
---
+++ web/src/views/PickupLocator/PickupLocator.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Map from './components/Map';
+
+const PickupLocator = (): JSX.Element => {
+  return (
+    <Map />
+  );
+}
+
+export default PickupLocator;
