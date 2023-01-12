068af9dc210a5b6f7e02af10444fe483655252e9
Moshe Haiem <46703279+moshehaiem@users.noreply.github.com>
2023-01-11 18:47:05 -0800

    Setup frontend (#3)
    * add packages
    
    * setting up map
    
    Co-authored-by: Moshe Haiem <mhaiem9@gmail.com>

---
+++ web/src/views/Main/MainPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PickupLocator from '../PickupLocator/PickupLocator';
+
+const MainPage = (): JSX.Element => {
+  return (
+    <PickupLocator />
+  );
+}
+
+export default MainPage;
---
+++ web/src/views/PickupLocator/components/Map.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Map = (): JSX.Element => {
+  return (
+    <>Pickup locator</>
+  );
+}
+
+export default Map;
