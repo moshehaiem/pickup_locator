4ade3ec04f25421ad5f307661725a21b8d8f118d
Moshe Haiem <mhaiem9@gmail.com>
2023-01-22 22:51:12 -0800

    add logic to check if map ref exists

--- web/src/views/PickupGameLocatorPage/components/PickupGameMap.tsx
+++ web/src/views/PickupGameLocatorPage/components/PickupGameMap.tsx
@@ -1,18 +1,20 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { Map, MapProvider, Popup, useMap } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
  
 const PickupGameMap = (): JSX.Element => {
   const [showPopup, setShowPopup] = useState(false);
   const [popupCoordinates, setPopupCoordinates] = useState([0, 0]);
-  const { map } = useMap();
+  const { current: map } = useMap();
 
   const flyToClickedPoint = useCallback((clickedPoint: any): void => {
-    map.flyTo({
-      center: clickedPoint.geometry.coordinates,
-      zoom: 15
-    });
-  }, []);
+    if(map){
+      map.flyTo({
+        center: clickedPoint.geometry.coordinates,
+        zoom: 15
+      });
+    }
+  }, [map]);
 
   const handleCreatePickupLocation = useCallback((event: any): void => {
     setShowPopup(true);
@@ -22,16 +24,25 @@ const PickupGameMap = (): JSX.Element => {
   }, []);
 
   const handleMapClick = useCallback((event: any): void => {
-    const features = map.queryRenderedFeatures(event.point, {
-      layers: ['locations']
-    });
-    if (!features.length){
-      handleCreatePickupLocation(event);
-    } else{
-      const clickedPoint = features[0];
-      flyToClickedPoint(clickedPoint);
+    console.log(map);
+    if(map){
+      const features = map.queryRenderedFeatures(event.point, {
+        layers: ['locations']
+      });
+      if (!features.length){
+        handleCreatePickupLocation(event);
+      } else{
+        const clickedPoint = features[0];
+        flyToClickedPoint(clickedPoint);
+      }
     }
   }, [flyToClickedPoint, handleCreatePickupLocation, map]);
+
+  useEffect(() => {
+    if (!map) {
+      return undefined;
+    }
+  }, [map]);
 
   return (
     <MapProvider>
