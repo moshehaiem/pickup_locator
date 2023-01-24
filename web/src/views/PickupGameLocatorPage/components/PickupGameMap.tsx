27bc9753dd538a62b41e6114260bd5733aa73804
Moshe Haiem <mhaiem9@gmail.com>
2023-01-23 20:48:18 -0800

    start on adding popup create

--- web/src/views/PickupGameLocatorPage/components/PickupGameMap.tsx
+++ web/src/views/PickupGameLocatorPage/components/PickupGameMap.tsx
@@ -1,66 +1,74 @@
-import React, { useState, useCallback, useEffect } from 'react';
-import { Map, MapProvider, Popup, useMap } from 'react-map-gl';
+import React, { useState, useCallback, useMemo, useRef } from 'react';
+import { Map, MapRef, Marker, Popup } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import PickupGamePopup from './PickupGamePopup';
  
 const PickupGameMap = (): JSX.Element => {
-  const [showPopup, setShowPopup] = useState(false);
-  const [popupCoordinates, setPopupCoordinates] = useState([0, 0]);
-  const { current: map } = useMap();
+  const [popupInfo, setPopupInfo] = useState<any>(null);
+  const mapRef = useRef<MapRef | null>(null);
 
-  const flyToClickedPoint = useCallback((clickedPoint: any): void => {
-    if(map){
-      map.flyTo({
-        center: clickedPoint.geometry.coordinates,
-        zoom: 15
+  const location_markers = useMemo(
+    () =>
+      [
+        {"lat": 6, "lng": 9, "athletesPresent": 6, "athletesNeeded": 10, "startTime": new Date(),
+         "endTime": new Date(), "message": 'Come hoop we got good games!'}].map((location, index) => (
+        <Marker
+          key={`marker-${index}`}
+          longitude={location.lng}
+          latitude={location.lat}
+          anchor="bottom"
+          onClick={e => {
+            e.originalEvent.stopPropagation();
+            setPopupInfo(location);
+          }}
+        >
+        </Marker>
+      )),
+    []
+  );
+
+  const flyToClickedPoint = useCallback((lng: number, lat: number): void => {
+    if(mapRef && mapRef.current){
+      mapRef.current.flyTo({
+        center: [lng, lat] ,
+        zoom: 18
       });
     }
-  }, [map]);
+  }, [mapRef]);
 
-  const handleCreatePickupLocation = useCallback((event: any): void => {
-    setShowPopup(true);
-    setPopupCoordinates([event.point.x, event.point.y])
-    console.log('create pickup location');
-    console.log(event);
+  const handleCreatePickupLocation = useCallback((lng: number, lat: number): void => {
+    setPopupInfo({"lat": lat, "lng": lng});
   }, []);
 
   const handleMapClick = useCallback((event: any): void => {
-    console.log(map);
-    if(map){
-      const features = map.queryRenderedFeatures(event.point, {
-        layers: ['locations']
-      });
-      if (!features.length){
-        handleCreatePickupLocation(event);
-      } else{
-        const clickedPoint = features[0];
-        flyToClickedPoint(clickedPoint);
-      }
+    console.log(popupInfo ? 'exists' : 'does not exist');
+    setPopupInfo(null)
+    if(mapRef && mapRef.current){
+      flyToClickedPoint(event.lngLat.lng, event.lngLat.lat);
+      handleCreatePickupLocation(event.lngLat.lng, event.lngLat.lat);
     }
-  }, [flyToClickedPoint, handleCreatePickupLocation, map]);
+  }, [flyToClickedPoint, handleCreatePickupLocation, popupInfo]);
 
-  useEffect(() => {
-    if (!map) {
-      return undefined;
-    }
-  }, [map]);
+  console.log(popupInfo);
 
   return (
-    <MapProvider>
-      <Map
-        id="map"
-        mapboxAccessToken=''
-        style={{width: 600, height: 400}}
-        mapStyle="mapbox://styles/mapbox/streets-v9"
-        onClick={handleMapClick}>
-          {showPopup && (
-            <Popup longitude={popupCoordinates[0]} latitude={popupCoordinates[1]}
-              anchor="bottom"
-              onClose={() => setShowPopup(false)}>
-              Add event
-            </Popup>)
-          }
-        </Map>
-    </MapProvider>
+    <Map
+      ref={mapRef}
+      reuseMaps
+      id="map"
+      mapboxAccessToken=''
+      style={{width: 1000, height: 800}}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      onClick={handleMapClick}>
+      {location_markers}
+      {popupInfo && (
+        <Popup longitude={popupInfo.lng} latitude={popupInfo.lat}
+          anchor="top"
+          onClose={() => setPopupInfo(null)}>
+          <PickupGamePopup />
+        </Popup>)
+      }
+      </Map>
   );
 }
 
---
+++ web/src/views/PickupGameLocatorPage/components/PickupGamePopup.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'mapbox-gl/dist/mapbox-gl.css';
+ 
+const PickupGamePopup = (): JSX.Element => {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default PickupGamePopup;
