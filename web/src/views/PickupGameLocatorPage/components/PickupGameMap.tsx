a73a568076eab4f67631ae5d84d9bb9cdfd71b2d
Moshe Haiem <mhaiem9@gmail.com>
2023-01-24 22:13:27 -0800

    create popup

--- web/src/views/PickupGameLocatorPage/components/PickupGameMap.tsx
+++ web/src/views/PickupGameLocatorPage/components/PickupGameMap.tsx
@@ -4,68 +4,64 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import PickupGamePopup from './PickupGamePopup';
  
 const PickupGameMap = (): JSX.Element => {
-  const [popupInfo, setPopupInfo] = useState<any>(null);
+  const [location, setLocation] = useState<any>(null);
   const mapRef = useRef<MapRef | null>(null);
-
-  const location_markers = useMemo(
-    () =>
-      [
-        {"lat": 6, "lng": 9, "athletesPresent": 6, "athletesNeeded": 10, "startTime": new Date(),
-         "endTime": new Date(), "message": 'Come hoop we got good games!'}].map((location, index) => (
-        <Marker
-          key={`marker-${index}`}
-          longitude={location.lng}
-          latitude={location.lat}
-          anchor="bottom"
-          onClick={e => {
-            e.originalEvent.stopPropagation();
-            setPopupInfo(location);
-          }}
-        >
-        </Marker>
-      )),
-    []
-  );
 
   const flyToClickedPoint = useCallback((lng: number, lat: number): void => {
     if(mapRef && mapRef.current){
       mapRef.current.flyTo({
-        center: [lng, lat] ,
+        center: [lng, lat],
         zoom: 18
       });
     }
   }, [mapRef]);
 
-  const handleCreatePickupLocation = useCallback((lng: number, lat: number): void => {
-    setPopupInfo({"lat": lat, "lng": lng});
-  }, []);
-
-  const handleMapClick = useCallback((event: any): void => {
-    console.log(popupInfo ? 'exists' : 'does not exist');
-    setPopupInfo(null)
+  const handleMapClick = useCallback((loc: any): void => {
     if(mapRef && mapRef.current){
-      flyToClickedPoint(event.lngLat.lng, event.lngLat.lat);
-      handleCreatePickupLocation(event.lngLat.lng, event.lngLat.lat);
+      flyToClickedPoint(loc.longitude, loc.latitude);
+      setLocation(loc);
     }
-  }, [flyToClickedPoint, handleCreatePickupLocation, popupInfo]);
+  }, [flyToClickedPoint]);
 
-  console.log(popupInfo);
+  const locationMarkers = useMemo(
+    () =>
+      [
+        {"latitude": 6, "longitude": 9, "athletesPresent": 6, "athletesNeeded": 10, "startTime": new Date(),
+         "endTime": new Date(), "message": 'Come hoop we got good games!'}].map((loc, index) => (
+        <Marker
+          key={`marker-${index}`}
+          longitude={loc.longitude}
+          latitude={loc.latitude}
+          anchor="bottom"
+          onClick={e => {
+            e.originalEvent.stopPropagation();
+            handleMapClick(loc)
+          }}
+        >
+        </Marker>
+      )),
+    [handleMapClick]
+  );
+
+  console.log(location);
+
 
   return (
     <Map
       ref={mapRef}
       reuseMaps
       id="map"
-      mapboxAccessToken=''
+      mapboxAccessToken='pk.eyJ1IjoibW9zaGVoYWllbSIsImEiOiJjbGNzaGtkZTAwcHM1M3BteHpld2FpdDB1In0.8sgxSa400qyrZXwxXbQjiQ'
       style={{width: 1000, height: 800}}
       mapStyle="mapbox://styles/mapbox/streets-v9"
-      onClick={handleMapClick}>
-      {location_markers}
-      {popupInfo && (
-        <Popup longitude={popupInfo.lng} latitude={popupInfo.lat}
+      onClick={(e) => handleMapClick({"latitude": e.lngLat.lat, "longitude": e.lngLat.lng, "athletesPresent": null, "athletesNeeded": null, "startTime": null,
+      "endTime": null, "message": null})}>
+      {locationMarkers}
+      {location && (
+        <Popup longitude={location.longitude} latitude={location.latitude}
           anchor="top"
-          onClose={() => setPopupInfo(null)}>
-          <PickupGamePopup />
+          onClose={() => setLocation(null)}>
+          <PickupGamePopup location={location} />
         </Popup>)
       }
       </Map>
--- web/src/views/PickupGameLocatorPage/components/PickupGamePopup.tsx
+++ web/src/views/PickupGameLocatorPage/components/PickupGamePopup.tsx
@@ -1,11 +1,63 @@
-import React from 'react';
-import 'mapbox-gl/dist/mapbox-gl.css';
- 
-const PickupGamePopup = (): JSX.Element => {
-  return (
-    <div>
+import React, { useCallback, useState } from 'react';
 
-    </div>
+const PickupGamePopup = ({ location }: any): JSX.Element => {
+  const [loc, setLoc] = useState(location);
+
+  const handleSubmit = useCallback((): void => {
+    console.log(loc);
+  }, [loc]);
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <fieldset>
+      <label>Athletes Present
+      <input 
+        type="number" 
+        name="athletesPresent" 
+        value={loc.athletesPresent || ""} 
+        onChange={(event) => setLoc({...loc, [event.target.name]: event.target.valueAsNumber})}
+      />
+      </label>
+      <br />
+      <label>Athletes Needed
+        <input 
+          type="number" 
+          name="athletesNeeded" 
+          value={loc.athletesNeeded || ""} 
+          onChange={(event) => setLoc({...loc, [event.target.name]: event.target.valueAsNumber})}
+        />
+        </label>
+        <br />
+        <label>Start Time
+        <input 
+          type="datetime-local" 
+          name="startTime" 
+          value={loc.startTime || ""} 
+          onChange={(event) => setLoc({...loc, [event.target.name]: event.target.valueAsDate})}
+        />
+        </label>
+        <br />
+        <label>End Time
+        <input 
+          type="datetime-local" 
+          name="endTime" 
+          value={loc.endTime || ""} 
+          onChange={(event) => setLoc({...loc, [event.target.name]: event.target.valueAsDate})}
+        />
+        </label>
+        <br />
+        <label>Message
+        <input 
+          type="text" 
+          name="message" 
+          value={loc.message || ""} 
+          onChange={(event) => setLoc({...loc, [event.target.name]: event.target.value})}
+        />
+        </label>
+        <br />
+        <input type="submit" />
+      </fieldset>
+    </form>
   );
 }
 
