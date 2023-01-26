7c317233348016b65f1909386074293181a300d1
Moshe Haiem <mhaiem9@gmail.com>
2023-01-25 20:14:45 -0800

    get current location

--- web/src/views/PickupGameLocatorPage/components/PickupGameMap.tsx
+++ web/src/views/PickupGameLocatorPage/components/PickupGameMap.tsx
@@ -1,10 +1,11 @@
-import React, { useState, useCallback, useMemo, useRef } from 'react';
-import { Map, MapRef, Marker, Popup } from 'react-map-gl';
+import React, { useState, useCallback, useMemo, useRef, useEffect } from 'react';
+import { GeolocateControl, Map, MapRef, Marker, Popup } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import PickupGamePopup from './PickupGamePopup';
  
 const PickupGameMap = (): JSX.Element => {
   const [location, setLocation] = useState<any>(null);
+  const [viewport, setViewport] = useState<any>(null);
   const mapRef = useRef<MapRef | null>(null);
 
   const flyToClickedPoint = useCallback((lng: number, lat: number): void => {
@@ -26,45 +27,68 @@ const PickupGameMap = (): JSX.Element => {
   const locationMarkers = useMemo(
     () =>
       [
-        {"latitude": 6, "longitude": 9, "athletesPresent": 6, "athletesNeeded": 10, "startTime": new Date(),
-         "endTime": new Date(), "message": 'Come hoop we got good games!'}].map((loc, index) => (
-        <Marker
-          key={`marker-${index}`}
-          longitude={loc.longitude}
-          latitude={loc.latitude}
-          anchor="bottom"
-          onClick={e => {
-            e.originalEvent.stopPropagation();
-            handleMapClick(loc)
-          }}
+        {
+          "latitude": 34.05794190025496, "longitude": -118.39487760130953, "athletesPresent": 6,
+          "athletesNeeded": 10, "startTime": new Date().toISOString().substring(0, 16),
+          "endTime": new Date().toISOString().substring(0, 16), "message": 'Come hoop we got good games!'
+        }
+      ].map((loc, index) => (
+      <Marker
+        key={`marker-${index}`}
+        longitude={loc.longitude}
+        latitude={loc.latitude}
+        anchor="bottom"
+        onClick={e => {
+          e.originalEvent.stopPropagation();
+          handleMapClick(loc)
+        }}
         >
-        </Marker>
+      </Marker>
       )),
     [handleMapClick]
   );
 
-  console.log(location);
+  useEffect(() => {
+    if(!viewport){
+      navigator.geolocation.getCurrentPosition(pos => {
+        setViewport({
+          latitude: pos.coords.latitude,
+          longitude: pos.coords.longitude,
+          zoom: 18
+        });
+      }, () => setViewport({
+        latitude: 10,
+        longitude: 10,
+        zoom: 18
+      }));
+    }
+  }, [viewport]);
 
 
   return (
-    <Map
-      ref={mapRef}
-      reuseMaps
-      id="map"
-      mapboxAccessToken='pk.eyJ1IjoibW9zaGVoYWllbSIsImEiOiJjbGNzaGtkZTAwcHM1M3BteHpld2FpdDB1In0.8sgxSa400qyrZXwxXbQjiQ'
-      style={{width: 1000, height: 800}}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
-      onClick={(e) => handleMapClick({"latitude": e.lngLat.lat, "longitude": e.lngLat.lng, "athletesPresent": null, "athletesNeeded": null, "startTime": null,
-      "endTime": null, "message": null})}>
-      {locationMarkers}
-      {location && (
-        <Popup longitude={location.longitude} latitude={location.latitude}
-          anchor="top"
-          onClose={() => setLocation(null)}>
-          <PickupGamePopup location={location} />
-        </Popup>)
-      }
+    <div>
+    {viewport && (
+      <Map
+        ref={mapRef}
+        reuseMaps
+        initialViewState={viewport}
+        mapboxAccessToken=''
+        style={{width: 1000, height: 800}}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        onClick={(e) => handleMapClick({"latitude": e.lngLat.lat, "longitude": e.lngLat.lng, "athletesPresent": null, "athletesNeeded": null, "startTime": null,
+        "endTime": null, "message": null})}>
+        <GeolocateControl />
+        {locationMarkers}
+        {location && (
+          <Popup longitude={location.longitude} latitude={location.latitude}
+            anchor="top"
+            onClose={() => setLocation(null)}>
+            <PickupGamePopup location={location} />
+          </Popup>)
+        }
       </Map>
+      )}
+    </div>
   );
 }
 
--- web/src/views/PickupGameLocatorPage/components/PickupGamePopup.tsx
+++ web/src/views/PickupGameLocatorPage/components/PickupGamePopup.tsx
@@ -3,60 +3,61 @@ import React, { useCallback, useState } from 'react';
 const PickupGamePopup = ({ location }: any): JSX.Element => {
   const [loc, setLoc] = useState(location);
 
-  const handleSubmit = useCallback((): void => {
+  const handleSubmit = useCallback((event: any): void => {
+    event.preventDefault();
     console.log(loc);
   }, [loc]);
+  console.log(loc);
 
   return (
     <form onSubmit={handleSubmit}>
-      <fieldset>
-      <label>Athletes Present
+    <label>Athletes Present
+    <input 
+      type="number" 
+      name="athletesPresent" 
+      value={loc.athletesPresent || ""} 
+      onChange={(event) => setLoc({...loc, [event.target.name]: event.target.valueAsNumber})}
+    />
+    </label>
+    <br />
+    <label>Athletes Needed
       <input 
         type="number" 
-        name="athletesPresent" 
-        value={loc.athletesPresent || ""} 
+        name="athletesNeeded" 
+        value={loc.athletesNeeded || ""} 
         onChange={(event) => setLoc({...loc, [event.target.name]: event.target.valueAsNumber})}
       />
       </label>
       <br />
-      <label>Athletes Needed
-        <input 
-          type="number" 
-          name="athletesNeeded" 
-          value={loc.athletesNeeded || ""} 
-          onChange={(event) => setLoc({...loc, [event.target.name]: event.target.valueAsNumber})}
-        />
-        </label>
-        <br />
-        <label>Start Time
-        <input 
-          type="datetime-local" 
-          name="startTime" 
-          value={loc.startTime || ""} 
-          onChange={(event) => setLoc({...loc, [event.target.name]: event.target.valueAsDate})}
-        />
-        </label>
-        <br />
-        <label>End Time
-        <input 
-          type="datetime-local" 
-          name="endTime" 
-          value={loc.endTime || ""} 
-          onChange={(event) => setLoc({...loc, [event.target.name]: event.target.valueAsDate})}
-        />
-        </label>
-        <br />
-        <label>Message
-        <input 
-          type="text" 
-          name="message" 
-          value={loc.message || ""} 
-          onChange={(event) => setLoc({...loc, [event.target.name]: event.target.value})}
-        />
-        </label>
-        <br />
-        <input type="submit" />
-      </fieldset>
+      <label>Start Time
+      <input 
+        type="datetime-local" 
+        name="startTime" 
+        value={loc.startTime || ""} 
+        onInput={event => console.log(event)}
+        onChange={(event) => setLoc({...loc, [event.target.name]: event.target.valueAsDate})}
+      />
+      </label>
+      <br />
+      <label>End Time
+      <input 
+        type="datetime-local" 
+        name="endTime" 
+        value={loc.endTime || ""} 
+        onChange={(event) => setLoc({...loc, [event.target.name]: event.target.valueAsDate})}
+      />
+      </label>
+      <br />
+      <label>Message
+      <input 
+        type="text" 
+        name="message" 
+        value={loc.message || ""} 
+        onChange={(event) => setLoc({...loc, [event.target.name]: event.target.value})}
+      />
+      </label>
+      <br />
+      <input type="submit" />
     </form>
   );
 }
