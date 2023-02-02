d7d8eedeef1847d8b6bafbd49e3f3ab26bdb9cce
Moshe Haiem <mhaiem9@gmail.com>
2023-02-01 19:03:06 -0800

    start retrieving locations by coord bounds

--- server/location/views.py
+++ server/location/views.py
@@ -6,3 +6,23 @@ from .models import Location
 class LocationViewSet(viewsets.ModelViewSet):
     serializer_class = LocationSerializer
     queryset = Location.objects.all()
+
+    def get_queryset(self):
+        queryset = Location.objects.all()
+        neLatitude = self.request.query_params["neLatitude"]
+        neLongitude = self.request.query_params["neLongitude"]
+        swLatidude = self.request.query_params["swLatidude"]
+        swLongitude = self.request.query_params["swLongitude"]
+        if neLatitude:
+            queryset = queryset.filter(latitude__lte=int(neLatitude))
+        if neLongitude:
+            queryset = queryset.filter(longitude__lte=int(neLongitude))
+        if swLatidude:
+            queryset = queryset.filter(latitude__gte=int(swLatidude))
+        if swLongitude:
+            queryset = queryset.filter(longitude__gte=int(swLongitude))
+
+        return queryset
+
+    def list(self, request, *args, **kwargs):
+        return super().list(request, args, kwargs)
